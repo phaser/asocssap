e88f81bc7707547b27f2c576c4030c62596d1359
phaser <cristian.bidea@gmail.com>
2014-03-13 23:17:12 +0200

    Added KRAL and some help

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ asocssap
 ========
 
 All Sort Of Code Snippets Solving Algorithms Problems
+
+Compiling
+=========
+
+To see a list of target and a short help on how to build issue the following command:
+
+    cmake -P KRAL/builder.cmake
+
+To generate the project a commmand like the following will do it:
+
+    cmake -DT=testbed -DGENERATE=1 -P KRAL/builder.cmake
+
+To compile it:
+
+    cmake -DT=testbed -DCOMPILE=1 -P KRAL/builder.cmake
+
+To build a target in a single step:
+
+    cmake -DT=testbed -DBUILD=1 -P KRAL/builder.cmake
---
+++ config.cmake
@@ -0,0 +1,27 @@
+cmake_minimum_required (VERSION 2.8)
+include (KRAL/config.cmake)
+
+set (KRAL_PATH      "${CMAKE_CURRENT_LIST_DIR}/KRAL/")
+set (PACKAGE_DIRS   "${CMAKE_CURRENT_LIST_DIR}/packages;${CMAKE_CURRENT_LIST_DIR}/thirdparty")
+set (PROJECTS_ROOT_DIR  "${CMAKE_CURRENT_LIST_DIR}/projects")
+
+# Default target
+set (PLATFORM_DEFAULT           "linux")
+set (PROJECT_TYPE_DEFAULT       "Unix Makefiles")
+
+# === TARGET
+set (CTARGET "SDLDemo-osx")
+list (APPEND TARGETS ${CTARGET})
+copy_target ("DEFAULT" ${CTARGET})
+set (TARGET_NAME_${CTARGET}     "${CTARGET}")
+set (PLATFORM_${CTARGET}        "osx")
+set (PROJECT_DIR_${CTARGET}     "${CMAKE_CURRENT_LIST_DIR}/testbed")
+set (BUILD_DIR_${CTARGET}       "testbed-osx")
+set (PROJECT_TYPE_${CTARGET}    "Xcode")
+set (CUSTOM_ARGS_${CTARGET}     "-Wdev")
+set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DRUNTIME_SUFFIX=/Debug")
+set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DIOS_SDK_PATH=\"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs\"")
+set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DTARGETSDK=MacOSX10.8.sdk ")
+set (CUSTOM_ARGS_${CTARGET}     "${CUSTOM_ARGS_${CTARGET}} -DDEST_PREFIX=Debug ")
+set (BUILD_TYPE_${CTARGET}      "debug")
+
