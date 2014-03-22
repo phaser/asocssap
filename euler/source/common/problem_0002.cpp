454a5265c71647183baf7cf51e4d1a8a9f1b4bdc
phaser <cristian.bidea@gmail.com>
2014-03-23 00:23:30 +0200

    adding solution to problem_0002

--- euler/source/common/problem_0002.cpp
+++ euler/source/common/problem_0002.cpp
@@ -3,19 +3,18 @@
 
 namespace problem_0002
 {
-struct fibo_sum
-{
-    uint64_t& sum;
-    uint64_t f1;
-    uint64_t f2;
-    void operator() (uint64_t term)
-    {
-         
-    }
-};
-
 uint64_t solve_problem()
 {
-    return 0;
+    uint64_t f1 = 1;
+    uint64_t f2 = 2;
+    uint64_t sum = 0;
+    while (f2 < 4000000)
+    {
+        if (f2 % 2 == 0) sum += f2;
+        uint64_t tmp = f2;
+        f2 += f1;
+        f1 = tmp;
+    }
+    return sum;
 }
 }  // namespace problem_0002
