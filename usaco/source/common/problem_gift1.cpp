b396ac54b0516946579c8c508237dc4d7567c8b8
Cristian Bidea <cristi@king.com>
2014-07-21 17:26:59 +0300

    new problem

---
+++ usaco/source/common/problem_gift1.cpp
@@ -0,0 +1,19 @@
+/*
+ID: cristia26
+PROG: gift1
+LANG: C++11
+*/
+#include <cstdio>
+#include <string.h>
+#include <stdint.h>
+#include <map>
+#define FILENAME "gift1"
+
+#ifndef TESTS
+int main()
+{
+    FILE *fin = fopen(FILENAME ".in", "r");
+    FILE *fout = fopen(FILENAME ".out", "w");
+    return 0;
+}
+#endif
