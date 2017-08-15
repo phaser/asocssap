3ee1dc38e78968892dec87130ca5ca7396a6ea35
Cristian Bidea <cristian.bidea@gmail.com>
2017-08-15 23:30:12 +0300

    Solved CF 549A

--- topcoder/topcoder/CF_496A.cs
+++ topcoder/topcoder/CF_496A.cs
@@ -8,6 +8,16 @@ class CF_496A
 {
     static int Main(string[] args)
     {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string line = Console.ReadLine();
+        string[] snums = line.Split(new char[] { ' ' });
+        List<int> nums = new List<int>();
+        foreach (var snum in snums)
+        {
+            nums.Add(Convert.ToInt32(snum));
+        }
+        int result = new CF_496A().solve(nums.ToArray());
+        Console.WriteLine(result);
         return 0;
     }
 
@@ -21,10 +31,30 @@ class CF_496A
             pn = holds[i];
         }
 
-        return 0;
+        int min = 1;
+        for (int i = 2; i < diffs.Length; i++)
+        {
+            if (diffs[i] < diffs[min])
+                min = i;
+            else if (diffs[i] == diffs[min] 
+                     && i+1 < diffs.Length 
+                     && min+1 < diffs.Length 
+                     && (diffs[i] + diffs[i + 1]) < (diffs[min] + diffs[min + 1]))
+                min = i;
+        }
+        if (min == (diffs.Length - 1)) min--;
+        diffs[min] += diffs[min + 1];
+        int max = diffs[1];
+        for (int i = 2; i < diffs.Length; i++)
+        {
+            if (diffs[i] > max)
+                max = diffs[i];
+        }
+        return max;
     }
 }
 
+#if !ONLINE_JUDGE
 namespace CodeForces
 {
     using NUnit.Framework;
@@ -33,10 +63,54 @@ namespace CodeForces
     class Test_CF_496A
     {
         [Test]
-        void Test1()
+        public void Test1()
         {
             int[] nums = { 1, 4, 6 };
             Assert.AreEqual(5, new CF_496A().solve(nums));
         }
+
+        [Test]
+        public void Test2()
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            Assert.AreEqual(2, new CF_496A().solve(nums));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int[] nums = { 1, 2, 3, 7, 8 };
+            Assert.AreEqual(4, new CF_496A().solve(nums));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            int[] nums = { 1, 4, 9, 16, 25, 36, 49, 64, 81, 100 };
+            Assert.AreEqual(19, new CF_496A().solve(nums));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            int[] nums = {
+                178, 186, 196, 209, 217,
+                226, 236, 248, 260, 273,
+                281, 291, 300, 309, 322,
+                331, 343, 357, 366, 377,
+                389, 399, 409, 419, 429,
+                442, 450, 459, 469, 477,
+                491, 501, 512, 524, 534,
+                548, 557, 568, 582, 593,
+                602, 616, 630, 643, 652,
+                660, 670, 679, 693, 707,
+                715, 728, 737, 750, 759,
+                768, 776, 789, 797, 807,
+                815, 827, 837, 849, 863,
+                873, 881, 890, 901, 910,
+                920, 932};
+            Assert.AreEqual(17, new CF_496A().solve(nums));
+        }
     }
 }
+#endif
---
+++ topcoder/topcoder/CF_549A.cs
@@ -0,0 +1,102 @@
+﻿using System;
+public class CF_549A
+{
+    static int Main(string[] args)
+    {
+        string nums = Console.ReadLine();
+        int nl = Convert.ToInt32(nums.Split()[0]);
+        string[] lines = new string[nl];
+        for (int i = 0; i < nl; i++)
+        {
+            lines[i] = Console.ReadLine();
+        }
+        Console.WriteLine(new CF_549A().solve(lines));
+        return 0;    
+    }
+
+	public int solve(string[] lines)
+	{
+		int count = 0;
+		int[] lc = new int[4];
+		for (int i = 0; i < lines.Length - 1; i++)
+		{
+			for (int j = 1; j < lines[i].Length; j++)
+			{
+                lc[0] = 0; lc[1] = 0; lc[2] = 0; lc[3] = 0;
+				adjustCount(lines[i][j], lc);
+				adjustCount(lines[i][j-1], lc);
+				adjustCount(lines[i+1][j], lc);
+				adjustCount(lines[i+1][j-1], lc);
+                if (lc[0] == 1 && lc[1] == 1 && lc[2] == 1 && lc[3] == 1)
+                {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+    private void adjustCount(char v, int[] lc)
+    {
+        switch (v)
+        {
+            case ('f'): { lc[0]++; break; }
+            case ('a'): { lc[1]++; break; }
+            case ('c'): { lc[2]++; break; }
+            case ('e'): { lc[3]++; break; }
+        }
+    }
+}
+
+#if !ONLINE_JUDGE
+namespace CodeForces
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class Test_CF_549A
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] lines = {
+                "xxxx",
+                "xfax",
+                "xcex",
+                "xxxx"
+            };
+
+            Assert.AreEqual(1, new CF_549A().solve(lines));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string[] lines = {
+                "xx",
+                "cf",
+                "ae",
+                "xx"
+            };
+            Assert.AreEqual(1, new CF_549A().solve(lines));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            string[] lines = {
+				"fac",
+                "cef"
+            };
+            Assert.AreEqual(2, new CF_549A().solve(lines));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            string[] lines = { "face" };
+            Assert.AreEqual(0, new CF_549A().solve(lines));
+        }
+    }
+}
+#endif
