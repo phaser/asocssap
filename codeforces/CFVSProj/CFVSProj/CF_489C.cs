341fa090767aa9be06c25c28f49b30ecfcaf6d2d
Cristian Bidea <cristian.bidea@gmail.com>
2017-08-22 23:28:37 +0300

    new problem solved

--- codeforces/CFVSProj/CFVSProj/CF_489C.cs
+++ codeforces/CFVSProj/CFVSProj/CF_489C.cs
@@ -1,5 +1,4 @@
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 public class CF_489C
 {
@@ -16,20 +15,55 @@ public class CF_489C
             }
         }
 
-        int md = s / m + s % m;
+        int md = s / m + (s % m == 0 ? 0 : 1);
         if (md > 9)
         {
             return "-1 -1";
         }
+        int[] min = new int[m];
+        int idx = m - 1; int sum = s;
+        while (sum > 0)
+        {
+            min[idx] = sum >= 9 ? 9 : sum;
+            sum -= min[idx];
+            idx--;
+        }
+        string smax = "";
+        for (int j = min.Length - 1; j >= 0; j--)
+        {
+            smax += min[j];
+        }
 
+        if (min[0] == 0)
+        {
+            min[0] = 1;
+            min[idx + 1]--;
+        }
+        string smin = "";
+        foreach (var d in min)
+        {
+            smin += d;
+        }
 
-        return "";
+        return "" + smin + " " + smax;
+    }
+
+    static int Main(string[] args)
+    {
+        string line = Console.ReadLine();
+        string[] snums = line.Split();
+        int m = Convert.ToInt32(snums[0]);
+        int s = Convert.ToInt32(snums[1]);
+        Console.WriteLine(new CF_489C().solve(m, s));
+        return 0;
     }
 }
 
 #if !ONLINE_JUDGE
 namespace CodeForces
 {
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+	
     [TestClass]
     public class Test_CF_489C
     {
@@ -56,7 +90,25 @@ namespace CodeForces
         [TestMethod]
         public void Test1()
         {
-            Assert.AreEqual("69 69", new CF_489C().solve(2, 15));
+            Assert.AreEqual("69 96", new CF_489C().solve(2, 15));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual("18 90", new CF_489C().solve(2, 9));
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual("9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999", new CF_489C().solve(100, 900));
+        }
+
+        [TestMethod]
+        public void FailedTest1()
+        {
+            Assert.AreEqual("899 998", new CF_489C().solve(3, 26));
         }
     }
 }
---
+++ topcoder/topcoder/CF_845B.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class CF_845A
+{
+	static int Main(string[] args)
+	{
+		string l = Console.ReadLine();
+		Console.WriteLine(new CF_845A().solve(l));
+		return 0;
+	}
+
+	public string solve(string t)
+	{
+		string t1 = t.Substring(0, 3);
+		string t2 = t.Substring(3, 3);
+		int s1 = t1[0] + t1[1] + t1[2];
+		int s2 = t2[0] + t2[1] + t2[2];
+		if (s1 == s2)
+		{
+			return "0";
+		}
+		int diff = Math.Abs(s1 - s2);
+		char[] ct1 = t1.ToCharArray();
+		char[] ct2 = t2.ToCharArray();
+		Array.Sort(ct1);
+		Array.Sort(ct2);
+        if (  '9' - ct1[0] >= diff
+           || '9' - ct2[0] >= diff
+           || ct1[2] - '0' >= diff
+           || ct2[2] - '0' >= diff)
+		{
+			return "1";
+		}
+		else
+        if (('9' - ct1[0]) + ('9' - ct1[1]) >= diff
+           || ('9' - ct2[0]) + ('9' - ct2[1]) >= diff
+           || (ct1[2] + ct1[1] - 2 * '0') >= diff
+           || (ct2[2] + ct2[1] - 2 * '0') >= diff)
+		{
+			return "2";
+		}
+		else
+			return "3";
+	}
+}
+
+#if !ONLINE_JUDGE
+namespace CodeForces
+{
+	using NUnit.Framework;
+
+	[TestFixture]
+	class Test_CF_845A
+    {
+		[Test]
+		public void Test1()
+		{
+            Assert.AreEqual("1", new CF_845A().solve("899888"));
+            Assert.AreEqual("1", new CF_845A().solve("899889"));
+            Assert.AreEqual("1", new CF_845A().solve("199880"));
+            Assert.AreEqual("2", new CF_845A().solve("123456"));
+		}
+	}
+}
+#endif
