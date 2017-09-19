d2fc73512f64fea6229a1814c1ccc41533222384
Cristian Bidea <cbidea@amberstudio.ro>
2017-09-19 10:46:51 +0300

    solved 131b

--- codeforces/CFVSProj/CFVSProj/CF_131C.cs
+++ codeforces/CFVSProj/CFVSProj/CF_131C.cs
@@ -4,24 +4,42 @@ public class CF_131C
 {
     public long solve(long n, long m, long t)
     {
-        return 0;
-    }
-
-    public long C(long n, long k)
-    {
-        long a = Math.Max(k, n - k);
-        long b = Math.Min(k, n - k);
-        return F(a + 1, n) / F(1, b);
-    }
-
-    public long F(long a, long b)
-    {
-        long ans = 1;
-        for (long i = a; i <= b; i++)
+        long b = Math.Min(n, t - 1);
+        long tsum = 0;
+        for (long i = 4; i <= b; i++)
         {
-            ans *= i;
+            tsum += GetBinCoeff(n, i) * GetBinCoeff(m, t - i);
         }
-        return ans;
+        return tsum;
+    }
+
+    static long GetBinCoeff(long N, long K)
+    {
+        // This function gets the total number of unique combinations based upon N and K.
+        // N is the total number of items.
+        // K is the size of the group.
+        // Total number of unique combinations = N! / ( K! (N - K)! ).
+        // This function is less efficient, but is more likely to not overflow when N and K are large.
+        // Taken from:  http://blog.plover.com/math/choose.html
+        //
+        if (K > N) return 0;
+        long r = 1;
+        long d;
+        for (d = 1; d <= K; d++)
+        {
+            r *= N--;
+            r /= d;
+        }
+        return r;
+    }
+
+    static void Main(string[] args)
+    {
+        string[] snums = Console.ReadLine().Split();
+        long n = Convert.ToInt64(snums[0]);
+        long m = Convert.ToInt64(snums[1]);
+        long t = Convert.ToInt64(snums[2]);
+        Console.WriteLine(new CF_131C().solve(n, m, t));
     }
 }
 
@@ -46,7 +64,21 @@ namespace CodeForces
             Assert.AreEqual(5, c);
             c = new CF_131C().C(5, 2);
             Assert.AreEqual(10, c);
+            c = new CF_131C().C(1, 1);
+            Assert.AreEqual(1, c);
         }
-	}
+
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual(3, new CF_131C().solve(4, 3, 5));
+        }
+
+        [TestMethod]
+        public void FailedTest1()
+        {
+            Assert.AreEqual(118264581548187697, new CF_131C().solve(30, 30, 30));
+        }
+    }
 }
 #endif
