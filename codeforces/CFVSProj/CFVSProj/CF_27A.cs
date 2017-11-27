2d05d84cb25c59b8a80c81c39a423cafea5c2146
Cristian Bidea <cbidea@amberstudio.ro>
2017-11-27 11:31:44 +0200

    new problem

---
+++ codeforces/CFVSProj/CFVSProj/CF_27A.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class CF_27A
+{
+    internal int solve(int[] v)
+    {
+        var existingMins = new HashSet<int>();
+        int min = int.MaxValue;
+        int max = int.MinValue;
+        foreach (var n in v)
+        {
+            existingMins.Add(n);
+            if (n > max)
+            {
+                max = n;
+            }
+        }
+        for (int i = 1; i <= max + 1; i++)
+        {
+            if (!existingMins.Contains(i))
+            {
+                min = i;
+                break;
+            }
+        }
+        return min;
+    }
+
+    public static void Main(string[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine());
+        int[] v = new int[n];
+        var lines = Console.ReadLine().Split();
+        for (int i = 0; i < n; i++)
+        {
+            v[i] = Convert.ToInt32(lines[i]);
+        }
+        Console.WriteLine(new CF_27A().solve(v));
+    }
+}
+
+#if !ONLINE_JUDGE
+namespace CodeForces
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Test_CF_27A
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual(3, new CF_27A().solve(new int[] { 1, 7, 2 }));
+            Assert.AreEqual(3, new CF_27A().solve(new int[] { 2, 7, 1 }));
+            Assert.AreEqual(3, new CF_27A().solve(new int[] { 7, 1, 2 }));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual(1, new CF_27A().solve(new int[] { 6, 4, 3, 5 }));
+        }
+    }
+}
+#endif
---
+++ codeforces/CFVSProj/CFVSProj/CF_556A.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+class CF_556A
+{
+    public int solve(string zo)
+    {
+        Stack<char> st = new Stack<char>();
+        foreach (var c in zo)
+        {
+            if (st.Count > 0)
+            {
+                if (st.Peek() != c)
+                {
+                    st.Pop();
+                } else {
+                    st.Push(c);
+                }
+            } else {
+                st.Push(c);
+            }
+        }
+        return st.Count;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.ReadLine();
+        Console.WriteLine(new CF_556A().solve(Console.ReadLine()));
+    }
+}
+
+#if !ONLINE_JUDGE
+namespace CodeForces
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class Test_CF_556A
+	{
+		[TestMethod]
+		public void Test1()
+		{
+            Assert.AreEqual(0, new CF_556A().solve("1100"));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual(1, new CF_556A().solve("01010"));
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual(6, new CF_556A().solve("11101111"));
+
+        }
+    }
+}
+#endif
