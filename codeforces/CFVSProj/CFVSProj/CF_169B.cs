5f81c4a55945d8777a2ddf44b57782fc18067246
Cristian Bidea <cbidea@amberstudio.ro>
2018-03-20 15:54:02 +0200

    new problem

---
+++ codeforces/CFVSProj/CFVSProj/CF_169B.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class CF_169B
+{
+    public string solve(char[] num, char[] s)
+    {
+        Array.Sort(s);
+        Array.Reverse(s);
+        int j = 0;
+        for (int i = 0; i < num.Length; i++)
+        {
+            if (num[i] < s[j])
+            {
+                num[i] = s[j];
+                j++;
+            }
+            if (j >= s.Length)
+            {
+                break;
+            }
+        }
+        return new string(num);
+    }
+
+    public static void Main()
+    {
+        string num = Console.ReadLine();
+        string s = Console.ReadLine();
+        Console.WriteLine(new CF_169B().solve(num.ToCharArray(), s.ToCharArray()));
+    }
+}
+
+#if !ONLINE_JUDGE
+namespace CodeForces
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Test_CF_169B
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.AreEqual("1124", new CF_169B().solve("1024".ToCharArray(), "010".ToCharArray()));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual("987", new CF_169B().solve("987".ToCharArray(), "1234567".ToCharArray()));
+        }
+    }
+}
+#endif
