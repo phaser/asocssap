247e2f92558a13a3527b6d7dc5884127bfdd05dc
Cristian Bidea <cristian.bidea@gmail.com>
2018-10-21 11:13:02 +0300

    Dir for BIT

---
+++ algorithms_stanford/binary-indexed-tree/binary-indexed-tree.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main(int argc, char** argv)
+{
+	int a = 17;
+	printf("%#0x\n%#0x\n", a, -a);
+	// 10001
+	// 01111
+	return 0;
+}
